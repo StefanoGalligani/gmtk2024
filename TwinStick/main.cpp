a666f1e335cab485beb46d1ca673f1b619f2d813
StefanoGalligani <stefanogalligani0987@gmail.com>
2024-07-04 15:41:01 +0200

    Refactored main

--- TwinStick/main.cpp
+++ TwinStick/main.cpp
@@ -19,127 +19,168 @@ float resy = 720;
 float shadowResx = 2048;
 float shadowResy = 2048;
 
-void onWindowResize(int x, int y) {
-    resx = x;
-    resy = y;
-    renderer.SetResolution(resx, resy);
-    mainCamera.SetPerspective(1.5f, (float)resx / resy, 0.05f, 3000.0f);
-}
+namespace sg {
+    class sgGame {
+    public:
+        void run() {
+            if (!initApplication()) return;
+            mainLoop();
+            cleanup();
+        }
 
-void onSpaceKeyPressed(int mods) {
-    showTriangulation = !showTriangulation;
-    renderer.SetShowTriangulation(showTriangulation);
-}
+    private:
+        bool initApplication() {
+            if (renderer.InitWindow("Quarto", resx, resy) < 0) return false;
+            renderer.InitPrograms();
+            BindInputs();
+            InitObjects();
 
-void onEscKeyPressed(int mods) {
-    glfwDestroyWindow(renderer.GetWindow());
-}
+            renderer.SetAmbientLight(0.5f);
+            renderer.SetupShadows(&spotLight, shadowResx, shadowResy);
+            renderer.SetSkybox("res/skybox/posx.png", "res/skybox/negx.png", "res/skybox/posy.png",
+                "res/skybox/negy.png", "res/skybox/posz.png", "res/skybox/negz.png");
+            return true;
+        }
 
-void onLeftMouseButtonClick(int mods) {
-    pressedL = true;
-    pressedCTRL = (mods & GLFW_MOD_CONTROL) > 0;
-}
+        void mainLoop() {
+            printf("Starting rendering\n");
+            while (!glfwWindowShouldClose(renderer.GetWindow()))
+            {
+                renderer.RenderFrame();
+                glfwPollEvents();
+            }
+        }
 
-void onLeftMouseButtonRelease(int mods) {
-    pressedL = false;
-}
+        void cleanup() {
+            printf("Terminating");
+            glfwTerminate();
+        }
 
-void onRightMouseButtonClick(int mods) {
-    pressedR = true;
-    pressedCTRL = (mods & GLFW_MOD_CONTROL) > 0;
-}
+        void InitObjects() {
+            printf("Initializing objects\n");
 
-void onRightMouseButtonRelease(int mods) {
-    pressedR = false;
-}
+            lightObj.LoadModelFromObj("res/light/light.obj");
+            lightObj.SetLocalPosition(0, 30, 60);
 
-void onMouseDrag(double xpos, double ypos) {
-    if (pressedCTRL) {
-        if (pressedL) {
-            double movex = (xpos - prevx) / resx;
-            double movey = -(ypos - prevy) / resy;
-            lightObj.RotateAround(glm::vec3(0, 1, 0), glm::vec3(0, 0, 0), movex);
-            lightObj.RotateAround(-lightObj.Right(), glm::vec3(0, 0, 0), movey);
+            mainCamera.SetPerspective(1.5f, (float)resx / resy, 0.05f, 3000.0f);
+            mainCamera.SetLocalPosition(glm::vec3(0, 30, 55));
+            mainCamera.LookAt(glm::vec3(0, 0, 0));
+
+            spotLight.SetPerspective(1.0f, (float)shadowResx / shadowResy, 1.0f, 1000.0f);
             spotLight.SetLocalPosition(lightObj.GetLocalPosition());
             spotLight.LookAt(glm::vec3(0, 0, 0));
+
+            renderer.AddObject(&lightObj);
+            renderer.SetMainCamera(&mainCamera);
+            renderer.SetSpotLight(&spotLight);
+
+            board.Init(&renderer);
+
+            printf("Buffers ready\n");
         }
-        if (pressedR) {
-            float movey = (ypos - prevy) / resy;
-            lightObj.Translate(lightObj.GetLocalPosition() * movey);
-            spotLight.SetLocalPosition(lightObj.GetLocalPosition());
-            spotLight.LookAt(glm::vec3(0, 0, 0));
+
+        void BindInputs() {
+            BindInput(sg::Key_Esc_Down, onEscKeyPressed);
+            BindInput(sg::Key_Space_Down, onSpaceKeyPressed);
+            BindInput(sg::Mouse_Left_Down, onLeftMouseButtonClick);
+            BindInput(sg::Mouse_Left_Up, onLeftMouseButtonRelease);
+            BindInput(sg::Mouse_Right_Down, onRightMouseButtonClick);
+            BindInput(sg::Mouse_Right_Up, onRightMouseButtonRelease);
+            BindInput(sg::Mouse_Position, onMouseDrag);
+            BindInput(sg::Window_Resize, onWindowResize);
         }
-    }
-    else {
-        if (pressedL) {
-            double movex = -(xpos - prevx) / resx;
-            double movey = (ypos - prevy) / resy;
-            mainCamera.RotateAround(glm::vec3(0, 1, 0), glm::vec3(0, 0, 0), movex);
-            mainCamera.RotateAround(-mainCamera.Right(), glm::vec3(0, 0, 0), movey);
-            mainCamera.LookAt(glm::vec3(0, 0, 0));
+
+        void BindInput(int cmd, sgKeyOrMouseFun callback) {
+            sg::InputManager::Instance()->BindInput(renderer.GetWindow(), cmd, callback);
         }
-        if (pressedR) {
-            float movey = (ypos - prevy) / resy;
-            mainCamera.Translate(mainCamera.GetLocalPosition() * movey);
-            mainCamera.LookAt(glm::vec3(0, 0, 0));
+        void BindInput(int cmd, sgCursorPosFun callback) {
+            sg::InputManager::Instance()->BindInput(renderer.GetWindow(), cmd, callback);
         }
-    }
-    prevx = xpos;
-    prevy = ypos;
-}
+        void BindInput(int cmd, sgWindowSizeFun callback) {
+            sg::InputManager::Instance()->BindInput(renderer.GetWindow(), cmd, callback);
+        }
 
-void InitObjects() {
-    printf("Initializing objects\n");
+        static void onEscKeyPressed(int mods) {
+            glfwDestroyWindow(renderer.GetWindow());
+        }
 
-    lightObj.LoadModelFromObj("res/light/light.obj");
-    lightObj.SetLocalPosition(0, 30, 60);
+        static void onSpaceKeyPressed(int mods) {
+            showTriangulation = !showTriangulation;
+            renderer.SetShowTriangulation(showTriangulation);
+        }
 
-    mainCamera.SetPerspective(1.5f, (float)resx / resy, 0.05f, 3000.0f);
-    mainCamera.SetLocalPosition(glm::vec3(0, 30, 55));
-    mainCamera.LookAt(glm::vec3(0, 0, 0));
+        static void onWindowResize(int x, int y) {
+            resx = x;
+            resy = y;
+            renderer.SetResolution(resx, resy);
+            mainCamera.SetPerspective(1.5f, (float)resx / resy, 0.05f, 3000.0f);
+        }
 
-    spotLight.SetPerspective(1.0f, (float)shadowResx / shadowResy, 1.0f, 1000.0f);
-    spotLight.SetLocalPosition(lightObj.GetLocalPosition());
-    spotLight.LookAt(glm::vec3(0, 0, 0));
+        static void onLeftMouseButtonClick(int mods) {
+            pressedL = true;
+            pressedCTRL = (mods & GLFW_MOD_CONTROL) > 0;
+        }
 
-    renderer.AddObject(&lightObj);
-    renderer.SetMainCamera(&mainCamera);
-    renderer.SetSpotLight(&spotLight);
+        static void onLeftMouseButtonRelease(int mods) {
+            pressedL = false;
+        }
 
-    board.Init(&renderer);
+        static void onRightMouseButtonClick(int mods) {
+            pressedR = true;
+            pressedCTRL = (mods & GLFW_MOD_CONTROL) > 0;
+        }
 
-    printf("Buffers ready\n");
-}
+        static void onRightMouseButtonRelease(int mods) {
+            pressedR = false;
+        }
 
-void BindInputs() {
-    sg::InputManager::Instance()->BindInput(renderer.GetWindow(), sg::Key_Esc_Down, onEscKeyPressed);
-    sg::InputManager::Instance()->BindInput(renderer.GetWindow(), sg::Key_Space_Down, onSpaceKeyPressed);
-    sg::InputManager::Instance()->BindInput(renderer.GetWindow(), sg::Mouse_Left_Down, onLeftMouseButtonClick);
-    sg::InputManager::Instance()->BindInput(renderer.GetWindow(), sg::Mouse_Left_Up, onLeftMouseButtonRelease);
-    sg::InputManager::Instance()->BindInput(renderer.GetWindow(), sg::Mouse_Right_Down, onRightMouseButtonClick);
-    sg::InputManager::Instance()->BindInput(renderer.GetWindow(), sg::Mouse_Right_Up, onRightMouseButtonRelease);
-    sg::InputManager::Instance()->BindInput(renderer.GetWindow(), sg::Mouse_Position, onMouseDrag);
-    sg::InputManager::Instance()->BindInput(renderer.GetWindow(), sg::Window_Resize, onWindowResize);
+        static void onMouseDrag(double xpos, double ypos) {
+            if (pressedCTRL) {
+                if (pressedL) {
+                    double movex = (xpos - prevx) / resx;
+                    double movey = -(ypos - prevy) / resy;
+                    lightObj.RotateAround(glm::vec3(0, 1, 0), glm::vec3(0, 0, 0), movex);
+                    lightObj.RotateAround(-lightObj.Right(), glm::vec3(0, 0, 0), movey);
+                    spotLight.SetLocalPosition(lightObj.GetLocalPosition());
+                    spotLight.LookAt(glm::vec3(0, 0, 0));
+                }
+                if (pressedR) {
+                    float movey = (ypos - prevy) / resy;
+                    lightObj.Translate(lightObj.GetLocalPosition() * movey);
+                    spotLight.SetLocalPosition(lightObj.GetLocalPosition());
+                    spotLight.LookAt(glm::vec3(0, 0, 0));
+                }
+            }
+            else {
+                if (pressedL) {
+                    double movex = -(xpos - prevx) / resx;
+                    double movey = (ypos - prevy) / resy;
+                    mainCamera.RotateAround(glm::vec3(0, 1, 0), glm::vec3(0, 0, 0), movex);
+                    mainCamera.RotateAround(-mainCamera.Right(), glm::vec3(0, 0, 0), movey);
+                    mainCamera.LookAt(glm::vec3(0, 0, 0));
+                }
+                if (pressedR) {
+                    float movey = (ypos - prevy) / resy;
+                    mainCamera.Translate(mainCamera.GetLocalPosition() * movey);
+                    mainCamera.LookAt(glm::vec3(0, 0, 0));
+                }
+            }
+            prevx = xpos;
+            prevy = ypos;
+        }
+    };
 }
 
 int main(int argc, char* argv[]) {
-    if (renderer.InitWindow("Quarto", resx, resy) < 0) return -1;
-    renderer.InitPrograms();
-    BindInputs();
-    InitObjects();
+    sg::sgGame game;
 
-    renderer.SetAmbientLight(0.5f);
-    renderer.SetupShadows(&spotLight, shadowResx, shadowResy);
-    renderer.SetSkybox("res/skybox/posx.png", "res/skybox/negx.png", "res/skybox/posy.png", "res/skybox/negy.png", "res/skybox/posz.png", "res/skybox/negz.png");
-
-    printf("Starting rendering\n");
-    while (!glfwWindowShouldClose(renderer.GetWindow()))
-    {
-        renderer.RenderFrame();
-        glfwPollEvents();
+    try {
+        game.run();
+    }
+    catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
     }
 
-    printf("Terminating");
-    glfwTerminate();
-    return 0;
+    return EXIT_SUCCESS;
 }
